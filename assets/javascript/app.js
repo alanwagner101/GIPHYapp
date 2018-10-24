4317d95c6cf30df669d5346580a3583f7ad500ff
Alan Wagner <alanwagner101@gmail.com>
2018-10-24 12:10:45 -0500

    final version

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -57,8 +57,10 @@ function gifInfo() {
 
         $(".gif").on("click", function () {
 
+            console.log(this)
+
             var state = $(this).attr("data-state");
-        
+
             if (state === "still") {
                 $(this).attr("data-state", "animate")
                 $(this).attr("src", $(this).attr("data-animate"));
@@ -77,82 +79,84 @@ function gifInfo() {
 
 function SearchButton() {
 
-        var tag = $("#tag-input").val();
+    var tag = $("#tag-input").val();
+    console.log(tag)
 
-        var queryURL = ("https://api.giphy.com/v1/gifs/search?q=" + tag + "&api_key=5z9ka4r0we9hpxZ8lskGF9UYOQ2xBXGb&limit=10");
+    var queryURL = ("https://api.giphy.com/v1/gifs/search?q=" + tag + "&api_key=5z9ka4r0we9hpxZ8lskGF9UYOQ2xBXGb&limit=10");
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response2) {
-            // var response2.data = response2.data;
-            $(".gif-connector").empty();
-            $(".gif-view").empty();
-            console.log(response2)
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        var gifs = response.data;
+        $(".gif-connector").empty();
+        $(".gif-view").empty();
+        console.log(response)
 
-            var active2 = $("<li>");
-            active2.addClass("active");
-            active2.attr("data-target", "#carouselExampleIndicators");
-            active2.attr("data-slide-to", "0");
+        var active2 = $("<li>");
+        active2.addClass("active");
+        active2.attr("data-target", "#carouselExampleIndicators");
+        active2.attr("data-slide-to", "0");
 
-            $(".gif-connector").append(active2);
+        $(".gif-connector").append(active2);
 
-            var active1 = $("<div>");
-            active1.addClass("carousel-item active");
-            active1.append(
-                "<img class='d-block w-100 gif' src='" + response2.data[0].images.original_still.url + "' data-still='" + response2.data[0].images.original_still.url + "' data-animate='" + response2.data[0].images.original.url + "' data-state='still'>" +
+        var active1 = $("<div>");
+        active1.addClass("carousel-item active");
+        active1.append(
+            "<img class='d-block w-100 gif' src='" + gifs[0].images.original_still.url + "' data-still='" + gifs[0].images.original_still.url + "' data-animate='" + gifs[0].images.original.url + "' data-state='still'>" +
+            "<div class='carousel-caption d-none d-md-block'>" +
+            "<h5> " + gifs[0].title + " </h5>" + "<p>Rating : " + gifs[0].rating + " </p>" +
+            "</div>"
+        );
+
+        $(".gif-view").append(active1);
+
+
+        for (var j = 1; j < gifs.length; j++) {
+            var gifConnector = $("<li>");
+            gifConnector.attr("data-target", "#carouselExampleIndicators");
+            gifConnector.attr("data-slide-to", "0");
+
+            $(".gif-connector").append(gifConnector);
+
+            var gifView = $("<div>");
+            gifView.addClass("carousel-item");
+            gifView.append(
+                "<img class='d-block w-100 gif' src='" + gifs[j].images.original_still.url + "' data-still='" + gifs[j].images.original_still.url + "' data-animate='" + gifs[j].images.original.url + "' data-state='still'>" +
                 "<div class='carousel-caption d-none d-md-block'>" +
-                "<h5> " + response2.data[0].title + " </h5>" + "<p>Rating : " + response2.data[0].rating + " </p>" +
+                "<h5> " + gifs[j].title + " </h5>" + "<p>Rating : " + gifs[j].rating + " </p>" +
                 "</div>"
             );
 
-            $(".gif-view").append(active1);
+            $(".gif-view").append(gifView);
+        }
 
+    $(".gif").on("click", function () {
 
-            for (var j = 1; j < response2.data.length; j++) {
-                var gifConnector = $("<li>");
-                gifConnector.attr("data-target", "#carouselExampleIndicators");
-                gifConnector.attr("data-slide-to", "0");
+        console.log(this)
 
-                $(".gif-connector").append(gifConnector);
+        var state = $(this).attr("data-state");
 
-                var gifView = $("<div>");
-                gifView.addClass("carousel-item");
-                gifView.append(
-                    "<img class='d-block w-100 gif' src='" + response2.data[j].images.original_still.url + "' data-still='" + response2.data[j].images.original_still.url + "' data-animate='" + response2.data[j].images.original.url + "' data-state='still'>" +
-                    "<div class='carousel-caption d-none d-md-block'>" +
-                    "<h5> " + response2.data[j].title + " </h5>" + "<p>Rating : " + response2.data[j].rating + " </p>" +
-                    "</div>"
-                );
+        if (state === "still") {
+            $(this).attr("data-state", "animate")
+            $(this).attr("src", $(this).attr("data-animate"));
+        } else if (state === "animate") {
+            $(this).attr("data-state", "still")
+            $(this).attr("src", $(this).attr("data-still"));
+        };
 
-                $(".gif-view").append(gifView);
-            }
+    });
 
-        });
-
-        $(".gif").on("click", function () {
-
-            var state = $(this).attr("data-state");
-        
-            if (state === "still") {
-                $(this).attr("data-state", "animate")
-                $(this).attr("src", $(this).attr("data-animate"));
-            } else if (state === "animate") {
-                $(this).attr("data-state", "still")
-                $(this).attr("src", $(this).attr("data-still"));
-            };
-
-        });
+});
 
 }
 
-$("#Search").on("click", function() {
+$("#Search").on("click", function () {
     event.preventDefault();
 
     SearchButton()
 });
 
-SearchButton()
 
 function renderButtons() {
 
@@ -189,7 +193,7 @@ $(document).on("click", ".tag", gifInfo);
 
 renderButtons();
 
-$("#clear").on("click", function() {
+$("#clear").on("click", function () {
 
     $(".gif-connector").empty();
     $(".gif-view").empty();
